bb4464852c01b04e931a8bf8c6dcf4728ec93ffd
WellisonTiago <wellisontiago2012@gmail.com>
2017-03-07 10:45:22 -0300

    atualização

--- estacionamento/application/config/config.php
+++ estacionamento/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/aulas/estacionamento/controlador';
+$config['base_url'] = 'http://localhost/ProjetoFinalWebII/estacionamento/controlador';
 
 /*
 |--------------------------------------------------------------------------
